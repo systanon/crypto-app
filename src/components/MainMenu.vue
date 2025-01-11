ef7201fb44f20cd82b438b25a6004f1ba26994cf
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-01-11 19:56:23 +0100

    fix(MainMenu): align items

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,13 +1,13 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import UISelect  from '@/components/ui/select/UISelect.vue'
-import UIIcon from '@/components/ui/icon/UIIcon.vue'
+import UIButtonIcon from '@/components/ui/buttons/UIButtonIcon.vue'
 
 export default defineComponent({
   name: "MainMenu",
   emits: ['handleClick', 'setCurrentSymbol'],
   components: {
-    UIIcon,
+    UIButtonIcon,
     UISelect,
   },
   props: {
@@ -29,7 +29,7 @@ export default defineComponent({
 
 <template>
     <aside class="app-menu" :class="{ open: isNavOpen }">
-      <UIIcon name="close-circle" @click="$emit('handleClick')" />
+      <UIButtonIcon icon-name="close-circle" @click="$emit('handleClick')" />
            <UISelect :modelValue="symbol" :options="symbols"
       @update:modelValue="$emit('setCurrentSymbol', $event)"/>
     </aside>
@@ -39,10 +39,14 @@ export default defineComponent({
 <style scoped>
 
   .app-menu {
-    background-color: blue;
+    background-color: rgb(239, 232, 232);
     color: white;
-    padding: 20px;
+    padding: 1em;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
     position: absolute;
+    gap: 1em;
     height: 100%;
     transform: translateX(-100%);
     transition: transform 0.3s ease-in-out;
